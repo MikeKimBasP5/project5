aa16de56ca438597eca94b7d7c289afd5cf78be4
Mike <ps199685@summacollege.nl>
2022-06-21 10:35:06 +0200

    Close #14

--- Project5Laravel/app/Http/Controllers/ExercisesController.php
+++ Project5Laravel/app/Http/Controllers/ExercisesController.php
@@ -44,9 +44,9 @@ class ExercisesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(exercise $exercise)
     {
-        //
+        return $exercise;
     }
 
     /**
